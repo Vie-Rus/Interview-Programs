32e107ad0d6189b94868e1eafb3d9fb072db3bf0
Vie-Rus <102697792+Vie-Rus@users.noreply.github.com>
2022-05-23 11:14:48 -0400

    Update Palindrome.py

--- Palindrome.py
+++ Palindrome.py
@@ -2,9 +2,9 @@
 # Written by V I E R U S
 # This program is to see if your user input is a palindrome or not
 #----------------------------------------------------------------------------------
-
+#def function calls palidrome, parameters of user's inputted string
 def palindrome(Userstring):
-    if Userstring.lower() == Userstring[::-1].lower():      #Lower case your input and reverse it
+    if Userstring.lower() == Userstring[::-1].lower():      #Lower case your input and reverse it then compare if they are the same
         print(Userstring + ": This is a Palindrome")
     else:
         print(Userstring + ": This NOT is a Palindrome")    #If all else it won't be a palidrome
